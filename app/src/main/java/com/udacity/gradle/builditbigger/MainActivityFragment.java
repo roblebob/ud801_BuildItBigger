63f85702f825f7772b81c89c7134388442d09788
roblebob <robert.koppisch@gmail.com>
2022-04-28 12:07:47 +0100

    Step 0: completed

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 
 /**
@@ -23,14 +24,27 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+
+        MobileAds .initialize( requireContext(), initializationStatus -> {});
+
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
+
+        /*
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+        */
+
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        mAdView.loadAd(adRequest);
+
         return root;
     }
 }
