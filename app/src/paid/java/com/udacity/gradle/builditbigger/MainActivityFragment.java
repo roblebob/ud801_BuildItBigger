b52c85d138ae304753386a3bb740f7662815f4aa
roblebob <robert.koppisch@gmail.com>
2022-05-06 15:15:36 +0100

    Step 5: individuals flavors for java code

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,37 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ((Button) root.findViewById(R.id.fragment_main_button)) .setOnClickListener( (v) -> {
+            new EndpointsAsyncTask().execute(new Pair<Context, String>( requireContext(), "Manfred"));
+        });
+
+        return root;
+    }
+}
