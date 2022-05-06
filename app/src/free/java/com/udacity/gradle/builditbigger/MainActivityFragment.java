a9ae14cbbaca48a515fb648785c48c5fb812de94
roblebob <robert.koppisch@gmail.com>
2022-05-06 17:31:45 +0100

    Optional Tasks - Add Interstitial Ad: complete

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -2,17 +2,25 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 
 /**
@@ -20,8 +28,14 @@ import com.google.android.gms.ads.MobileAds;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String TAG = MainActivityFragment.class.getSimpleName();
+
+    InterstitialAd mInterstitialAd;
+
+
     public MainActivityFragment() {
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,28 +43,46 @@ public class MainActivityFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
 
-        MobileAds .initialize( requireContext(), initializationStatus -> {});
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        /*
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
-        */
         AdRequest adRequest = new AdRequest.Builder().build();
+
+        MobileAds .initialize( requireContext(), initializationStatus -> {});
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
         mAdView.loadAd(adRequest);
+
+
+        InterstitialAd.load(requireContext(),"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        Log.e(TAG, "--------> " + "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.e(TAG, "--------> " + loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
 
 
         ((Button) root.findViewById(R.id.fragment_main_button)) .setOnClickListener( (v) -> {
-            Context context =  requireContext();
 
-            new EndpointsAsyncTask().execute(new Pair<Context, String>(context, "Manfred"));
+            if (mInterstitialAd != null) {
+                mInterstitialAd.show(requireActivity());
+            } else {
+                Log.e("TAG", "--------> " + "The interstitial ad wasn't ready yet.");
+            }
 
-
+            Handler handler = new Handler();
+            handler.postDelayed(() ->
+                    new EndpointsAsyncTask().execute(new Pair<Context, String>( requireContext(), "Manfred"))
+                    , 2000
+            );
         });
 
 
